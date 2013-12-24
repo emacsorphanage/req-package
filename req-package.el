b5b02980249cf7aaad13d38da82b47421572d98e
Edward Knyshov <edvorg@gmail.com>
2013-12-25 01:59:17 +0700

    form target list for processing

--- req-package.el
+++ req-package.el
@@ -1,5 +1,8 @@
 (require 'use-package)
 
+(defvar req-package-targets nil
+  "list of packages to load")
+
 (defmacro req-package (name &rest params)
   `(let ((NAME ',name)
          (ARGS ',params))
@@ -7,7 +10,9 @@
      (if (eq (car ARGS) :require)
          (if (eq (cdr ARGS) nil)
              (error "invalid arguments list")
-           (eval (apply 'list 'use-package NAME (cddr ARGS))))
-       (eval (apply 'list 'use-package NAME ARGS)))))
+           (add-to-list 'req-package-targets
+                        (list NAME (cadr ARGS) (apply 'list 'use-package NAME (cddr ARGS)))))
+       (add-to-list 'req-package-targets
+                    (list NAME nil (apply 'list 'use-package NAME ARGS))))))
 
 (provide 'req-package)
