b13032b91d2b5af879e0035148822b02a18ba969
Edward Knyshov <edvorg@gmail.com>
2013-12-25 01:20:32 +0700

    untabify

--- req-package.el
+++ req-package.el
@@ -2,12 +2,12 @@
 
 (defmacro req-package (name &rest params)
   `(let ((NAME ',name)
-		 (ARGS ',params))
+         (ARGS ',params))
 
-	 (if (eq (car ARGS) :require)
-		 (if (eq (cdr ARGS) nil)
-			 (error "invalid arguments list")
-		   (eval (apply 'list 'use-package NAME (cddr ARGS))))
-	   (eval (apply 'list 'use-package NAME ARGS)))))
+     (if (eq (car ARGS) :require)
+         (if (eq (cdr ARGS) nil)
+             (error "invalid arguments list")
+           (eval (apply 'list 'use-package NAME (cddr ARGS))))
+       (eval (apply 'list 'use-package NAME ARGS)))))
 
 (provide 'req-package)
