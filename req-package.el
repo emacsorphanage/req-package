7bc33049175f8d8571cb8c37811d8fca9ab2bb9f
Edward Knyshov <edvorg@gmail.com>
2013-12-25 17:03:28 +0700

    form eval list

--- req-package.el
+++ req-package.el
@@ -1,7 +1,14 @@
 (require 'use-package)
+(require 'cl)
 
 (defvar req-package-targets nil
   "list of packages to load")
+
+(defvar req-package-eval-list nil
+  "preprocessed list of forms to eval")
+
+(defun req-package-wrap-reqs (reqs)
+  (if (atom reqs) (list reqs) reqs))
 
 (defmacro req-package (name &rest args)
   `(let* ((NAME ',name)
@@ -11,9 +18,21 @@
                        (eq (car ARGS) :require)
                        (if (null (cdr ARGS)) (error ERRMES) t)))
           (USEPACKARGS (if HASREQ (cddr ARGS) ARGS))
-          (REQS (if HASREQ (cadr ARGS) nil))
+          (REQS (if HASREQ (req-package-wrap-reqs (cadr ARGS)) nil))
           (TARGET (list NAME REQS (apply 'list 'use-package NAME USEPACKARGS))))
 
      (add-to-list 'req-package-targets TARGET)))
+
+(defun req-package-form-eval-list ()
+  (mapcar (lambda (target) (add-to-list 'req-package-eval-list (caddr target)))
+          req-package-targets))
+
+(defun req-package-eval ()
+  (mapcar (lambda (target) (eval target))
+          req-package-eval-list))
+
+(defun req-package-finish ()
+  (progn (req-package-form-eval-list)
+         (req-package-eval)))
 
 (provide 'req-package)
