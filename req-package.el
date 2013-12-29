8c949b03ebb9524fbd867383a6dc832b1f4a9a26
Edward Knyshov <edvorg@gmail.com>
2013-12-29 18:50:09 +0700

    indent doc

--- req-package.el
+++ req-package.el
@@ -57,23 +57,23 @@
 
 ;; Migrate from use-package
 
-;; Just replace all (use-package ...) with (req-package [:require DEPS] ...)
-;; and add (req-package-finish) at the end of your configuration file.
+;;    Just replace all (use-package ...) with (req-package [:require DEPS] ...)
+;;    and add (req-package-finish) at the end of your configuration file.
 
 ;; Note
 
-;; All use-package parameters are supported, see use-package manual for additional info.
+;;    All use-package parameters are supported, see use-package manual for additional info.
 
-;; Also there are possible troubles with deferred loading provided by use-package.
-;; If you want to use it, try defer all packages in one dependency tree.
+;;    Also there are possible troubles with deferred loading provided by use-package.
+;;    If you want to use it, try defer all packages in one dependency tree.
 
 ;; Changelog:
 
-;; v0.2-auto-fetch:
-;;   There is no need of explicit :ensure in your code now.
-;;   When you req-package it adds :ensure if package is available in your repos.
-;;   Also package deps :ensure'd automatically too.
-;;   just write (req-package pkg1 :require pkg2) and all you need will be installed.
+;;    v0.2-auto-fetch:
+;;       There is no need of explicit :ensure in your code now.
+;;       When you req-package it adds :ensure if package is available in your repos.
+;;       Also package deps :ensure'd automatically too.
+;;       Just write (req-package pkg1 :require pkg2) and all you need will be installed.
 
 ;;; Code:
 
