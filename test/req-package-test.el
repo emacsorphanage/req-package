36dd37c823b43129c6f9fd102b84bfc38025602d
Edward Knyshov <edvorg@gmail.com>
2015-09-12 22:34:45 +0300

    improve coverage

--- test/req-package-test.el
+++ test/req-package-test.el
@@ -11,4 +11,10 @@
   (should (equal '(6) (car (req-package-extract-arg :init '(:config 5 :init 6) nil))))
   (should (equal '(5) (car (req-package-extract-arg :config'(:config 5 :init 6) nil)))))
 
+(ert-deftest req-package-patch-config-test ()
+  (should (equal '(:config (req-package-loaded (quote foo)))
+                 (req-package-patch-config 'foo '())))
+  (should (equal '(:init 5 :config (req-package-loaded (quote foo)))
+                 (req-package-patch-config 'foo '(:init 5)))))
+
 (provide 'req-package-test)
