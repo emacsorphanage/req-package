82d9d88073c88902c0ce2442249b8e31b348f2f6
Edward Knyshov <edvorg@gmail.com>
2015-09-12 22:27:04 +0300

    add case

--- test/req-package-test.el
+++ test/req-package-test.el
@@ -8,6 +8,7 @@
   (should (equal '(1) (req-package-wrap-args '(1)))))
 
 (ert-deftest req-package-extract-arg-test ()
-  (should (equal '(6) (car (req-package-extract-arg :init '(:config 5 :init 6) nil)))))
+  (should (equal '(6) (car (req-package-extract-arg :init '(:config 5 :init 6) nil))))
+  (should (equal '(5) (car (req-package-extract-arg :config'(:config 5 :init 6) nil)))))
 
 (provide 'req-package-test)
