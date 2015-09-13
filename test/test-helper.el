3ca74d190a920805da1524194bc764a1e4e808a5
Edward Knyshov <edvorg@gmail.com>
2015-09-13 03:10:00 +0300

    Revert "refactor"
    This reverts commit d2e95aad5805541f4f81404202ec3b0f2ccfaa50.

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,13 +1,1 @@
-(when (require 'undercover nil t)
-  (undercover "req-package.el"))
-
-(defun with-debug-on-error (m fn)
-  (let* ((current debug-on-error)
-         (_ (setq debug-on-error m))
-         (result (funcall fn))
-         (_ (setq debug-on-error current)))
-    result))
-
-(put 'with-debug-on-error 'lisp-indent-function 'defun)
-
 (provide 'test-helper)
