2bc8a97e812c039250753fe4e62dba21c50db28f
Edward Knyshov <edvorg@gmail.com>
2015-09-17 11:07:38 +0300

    try to fix coverage

--- test/req-package-test.el
+++ test/req-package-test.el
@@ -2,9 +2,6 @@
 (require 'ert)
 (require 'ert-expectations)
 
-(when (require 'undercover nil t)
-  (undercover "req-package.el"))
-
 (defun with-debug-on-error (m fn)
   (let* ((current debug-on-error)
          (_ (setq debug-on-error m))
--- test/test-helper.el
+++ test/test-helper.el
@@ -1,1 +1,4 @@
 (provide 'test-helper)
+
+(when (require 'undercover nil t)
+  (undercover "req-package.el"))
